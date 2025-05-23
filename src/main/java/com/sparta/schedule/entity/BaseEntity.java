8bf239e34fd847882faad209576b95626eef7632
천세경 <jay45652@gmail.com>
2025-05-23 13:08:38 +0900

    Feat: BaseEntity 추가 및 @EnableJpaAuditing 설정 - entity 패키지에 BaseEntity 클래스 추가 - Application 실행 클래스에 @EnableJpaAuditing 어노테이션 추가

---
+++ src/main/java/com/sparta/schedule/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.sparta.schedule.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDate createdAt;
+
+    @LastModifiedBy
+    private String updatedAt;
+}
