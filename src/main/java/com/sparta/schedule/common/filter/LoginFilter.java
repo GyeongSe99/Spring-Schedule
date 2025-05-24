257ecbca3a0449a12b409b8eaf6ad3970c210b72
천세경 <jay45652@gmail.com>
2025-05-24 20:32:41 +0900

    Feat: LoginFilter 구현

---
+++ src/main/java/com/sparta/schedule/common/filter/LoginFilter.java
@@ -0,0 +1,40 @@
+package com.sparta.schedule.common.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginFilter implements Filter {
+
+    private static final String[] WHITE_LIST = {"/", "/user/signup/", "/login", "/logout"};
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        log.info("로그인 필터 로직 실행");
+
+        // WHITE_LIST 에 포함되지 않은 경우 -> 로그인
+        if (!isWhiteList(requestURI)) {
+            HttpSession session = httpRequest.getSession(false);
+            if (session == null || session.getAttribute("userId") == null) {
+                throw new RuntimeException("로그인 해주세요.");
+            }
+
+            log.info("로그인에 성공했습니다.");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean isWhiteList(String requestURI) {
+        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+    }
+}
